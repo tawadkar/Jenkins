1f57c10df1930c42ae77c16ecc1e3319c41e7be2
Tanmay Wadkar <tanmaywadkar7@gmail.com>
2019-01-17 22:50:11 +0530

    update

---
+++ src/com/jenkinsdemo/Jenkins_Demo.java
@@ -0,0 +1,11 @@
+package com.jenkinsdemo;
+
+public class Jenkins_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");		
+	}
+
+}
